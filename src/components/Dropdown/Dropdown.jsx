fd064af447d8ddb2688bcabad061619eb36c3387
ZVMarina <zvyagintsevame@yandex.ru>
2022-05-06 22:02:28 +0700

    create Dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,13 +1,28 @@
-import { useRef, useState } from "react";
-import styles from './Dropdown.module.css'
+import { useRef, useState } from 'react';
+import styles from './Dropdown.module.css';
+import icon from '../../images/icon.png'
 
 const Dropdown = () => {
     const dropdownRef = useRef(null);
     const [isActive, setIsActive] = useState(false);
-  
-    return (
-        <div className={styles.dropdown}>Dropdown</div>
-    );
-  };
 
-  export default Dropdown;
+    const onClick = () => setIsActive(!isActive);
+
+    return (
+        <div className={styles.dropdownMenu}>
+            <button onClick={onClick} className={styles.trigger}>
+                <span>Filter</span>
+                <img className={styles.icon} src={icon} alt="icon" />
+            </button>
+            <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+                <ul>
+                    <li>name</li>
+                    <li>distance</li>
+                    <li>quantity</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Dropdown;
