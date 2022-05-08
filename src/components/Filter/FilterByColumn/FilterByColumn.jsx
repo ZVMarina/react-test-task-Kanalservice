d44da117ec4e753c3a30e6d9e8bf196cf93bf54b
ZVMarina <zvyagintsevame@yandex.ru>
2022-05-08 10:48:51 +0700

    Selection of conditions depending on the column filter

--- src/components/Filter/FilterByColumn/FilterByColumn.jsx
+++ src/components/Filter/FilterByColumn/FilterByColumn.jsx
@@ -3,12 +3,8 @@ import Dropdown from "../../Dropdown/Dropdown";
 import styles from '../../Filter/Filter.module.css';
 
 const FilterByColumn = ({ filtersColumn, activeFilterColumn, selectActiveFilter }) => {
-   const dropdownComponent = useRef(null);
-   console.log(dropdownComponent);
-
    return (
       <Dropdown
-         ref={dropdownComponent}
          trigger=
          {
             <span>{activeFilterColumn?.name ? activeFilterColumn.name : 'Select column'}</span>
